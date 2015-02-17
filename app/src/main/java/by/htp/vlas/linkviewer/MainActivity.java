92e2298cd53a16efcb93b40a5dd21af330f53c58
Viktor Volisov <vlaselectronics@gmail.com>
2015-02-17 23:03:44 +0300

    it's work

--- app/src/main/java/by/htp/vlas/linkviewer/MainActivity.java
+++ app/src/main/java/by/htp/vlas/linkviewer/MainActivity.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.webkit.URLUtil;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -19,23 +24,75 @@ public class MainActivity extends Activity {
     @InjectView(R.id.link_address)
     EditText mLinkAddressView;
 
+    private final String URI_SCHEME_HTTP = "http://";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
+
+        setOnKeyListenerToView(mLinkAddressView);
     }
 
     @OnClick(R.id.btn_go)
     void btnGoAction() {
+        loadUrl();
+    }
+
+    private void loadUrl() {
+        String uriString = mLinkAddressView.getText().toString();
+        uriString = validateUriString(uriString);
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        String uriString = mLinkAddressView.getText().toString();
-        if(!TextUtils.isEmpty(uriString)) {
-            intent.setData(Uri.parse(uriString));
-            startActivity(intent);
+        if (TextUtils.isEmpty(uriString)) {
+            Toast.makeText(this, getString(R.string.msg_blank_link_address), Toast.LENGTH_SHORT)
+                    .show();
+            return;
         }
 
-
+        if (resolveActivities(intent)) {
+            intent.setData(Uri.parse(uriString));
+            this.startActivity(intent);
+        }
     }
+
+    private String validateUriString(String pUriString) {
+        if (!URLUtil.isValidUrl(pUriString)) {
+            Uri uri = Uri.parse(pUriString);
+            String uriScheme = uri.getScheme();
+            if (TextUtils.isEmpty(uriScheme)) {
+                pUriString = URI_SCHEME_HTTP + pUriString;
+            }
+        }
+        return pUriString;
+    }
+
+    private boolean resolveActivities(Intent pIntent) {
+        if (pIntent.resolveActivity(getPackageManager()) == null) {
+            Toast.makeText(this, getString(R.string.msg_not_resolved_activity), Toast.LENGTH_SHORT)
+                    .show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void setOnKeyListenerToView(View view) {
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
+                        keyCode == EditorInfo.IME_ACTION_DONE ||
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER
+                        ) {
+                    loadUrl();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+
 }
